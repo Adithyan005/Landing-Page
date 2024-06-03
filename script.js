a9fff98377f329d62ce938c28a07a777d1f68318
Adithyan <adithyanm.22cse@kongu.edu>
2024-06-03 16:45:53 +0530

    modified

--- script.js
+++ script.js
@@ -33,3 +33,29 @@ home.onmouseleave=()=>{
     let homeImg = home.querySelector('.home-img');
     homeImg.style.transform=`translateX(0px) translateY(0px)`;
 }
+
+let dataOffset=(24*60*60*1000)*2;
+let countDate = new Date().getTime()+dataOffset;
+function countDown(){
+    let now=new Date().getTime();
+    let gap=countDate-now;
+
+    let seconds=1000;
+    let minutes=seconds*60;
+    let hours=minutes*60;
+    let days=hours*24;
+
+    let d=Math.floor(gap/(days));
+    let h=Math.floor((gap%(days))/(hours));
+    let m=Math.floor((gap%(hours))/(minutes));
+    let s=Math.floor((gap%(minutes))/(seconds));
+
+    document.getElementById('day').innerText=d;
+    document.getElementById('hour').innerText=h;
+    document.getElementById('minute').innerText=m;
+    document.getElementById('second').innerText=s;
+}
+
+setInterval(function(){
+    countDown()
+},1000);
